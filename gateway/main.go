4dc2e8433e389b1f608bfd74f990968f13c76286
Siddharth Agarwal <siddharth13103410@gmail.com>
2025-06-07 12:55:04 +0000

    push register-errorBasedScaling-handler-API-route-authentication(customErrorScaling)

--- gateway/main.go
+++ gateway/main.go
@@ -189,6 +189,10 @@ func main() {
 	faasHandlers.ListFunctions = metrics.AddMetricsHandler(faasHandlers.ListFunctions, prometheusQuery)
 	faasHandlers.ScaleFunction = scaling.MakeHorizontalScalingHandler(handlers.MakeForwardingProxyHandler(reverseProxy, forwardingNotifiers, urlResolver, nilURLTransformer, serviceAuthInjector, nil))
 
+	// SA - Add the proxy for the error based scaling endpoint
+	errorBasedScaling := handlers.MakeNotifierWrapper(
+		handlers.MakeErrorBasedScalingHandler(prometheusQuery, faasHandlers.Alert, config.Namespace, 2, 1, 15), quietNotifier)
+
 	if credentials != nil {
 		faasHandlers.Alert =
 			auth.DecorateWithBasicAuth(faasHandlers.Alert, credentials)
@@ -214,6 +218,8 @@ func main() {
 			auth.DecorateWithBasicAuth(faasHandlers.NamespaceListerHandler, credentials)
 		faasHandlers.NamespaceMutatorHandler =
 			auth.DecorateWithBasicAuth(faasHandlers.NamespaceMutatorHandler, credentials)
+		errorBasedScaling =
+			auth.DecorateWithBasicAuth(errorBasedScaling, credentials)
 	}
 
 	r := mux.NewRouter()
@@ -226,6 +232,8 @@ func main() {
 	r.HandleFunc("/system/info", faasHandlers.InfoHandler).Methods(http.MethodGet)
 	r.HandleFunc("/system/telemetry", faasHandlers.TelemetryHandler).Methods(http.MethodGet)
 
+	// SA - Add the error based scaling endpoint both for scale up and scale down
+	r.HandleFunc("/system/scale-on-errors", errorBasedScaling).Methods(http.MethodPost)
 	r.HandleFunc("/system/alert", faasHandlers.Alert).Methods(http.MethodPost)
 
 	r.HandleFunc("/system/function/{name:["+NameExpression+"]+}", faasHandlers.FunctionStatus).Methods(http.MethodGet)
