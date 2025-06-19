78970461ae4d2f09fccfd6a4c13afb72b64bc69d
Siddharth Agarwal <siddharth13103410@gmail.com>
2025-06-19 14:30:38 +0000

    push add-random-seed-coldStart-assumption(versionScoring)

--- gateway/main.go
+++ gateway/main.go
@@ -8,6 +8,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand" // SA - Importing math/rand to use random number generation
 	"net/http"
 	"time"
 
@@ -27,7 +28,7 @@ import (
 const NameExpression = "-a-zA-Z_0-9."
 
 func main() {
-
+	rand.Seed(time.Now().UnixNano()) // SA - Seed the random number generator to ensure randomness in operations
 	osEnv := types.OsEnv{}
 	readConfig := types.ReadConfig{}
 	config, configErr := readConfig.Read(osEnv)
@@ -190,8 +191,8 @@ func main() {
 	faasHandlers.ScaleFunction = scaling.MakeHorizontalScalingHandler(handlers.MakeForwardingProxyHandler(reverseProxy, forwardingNotifiers, urlResolver, nilURLTransformer, serviceAuthInjector, nil))
 
 	// SA - Add the proxy for the error based scaling endpoint
-	errorBasedScaling := handlers.MakeNotifierWrapper(
-		handlers.MakeErrorBasedScalingHandler(prometheusQuery, faasHandlers.Alert, config.Namespace, 2, 1, 15), quietNotifier)
+	// errorBasedScaling := handlers.MakeNotifierWrapper(
+	// 	handlers.MakeErrorBasedScalingHandler(prometheusQuery, faasHandlers.Alert, config.Namespace, 2, 1, 15), quietNotifier)
 
 	if credentials != nil {
 		faasHandlers.Alert =
@@ -218,8 +219,8 @@ func main() {
 			auth.DecorateWithBasicAuth(faasHandlers.NamespaceListerHandler, credentials)
 		faasHandlers.NamespaceMutatorHandler =
 			auth.DecorateWithBasicAuth(faasHandlers.NamespaceMutatorHandler, credentials)
-		errorBasedScaling =
-			auth.DecorateWithBasicAuth(errorBasedScaling, credentials)
+		// errorBasedScaling =
+		// 	auth.DecorateWithBasicAuth(errorBasedScaling, credentials)
 	}
 
 	r := mux.NewRouter()
@@ -233,7 +234,7 @@ func main() {
 	r.HandleFunc("/system/telemetry", faasHandlers.TelemetryHandler).Methods(http.MethodGet)
 
 	// SA - Add the error based scaling endpoint both for scale up and scale down
-	r.HandleFunc("/system/scale-on-errors", errorBasedScaling).Methods(http.MethodPost)
+	// r.HandleFunc("/system/scale-on-errors", errorBasedScaling).Methods(http.MethodPost)
 	r.HandleFunc("/system/alert", faasHandlers.Alert).Methods(http.MethodPost)
 
 	r.HandleFunc("/system/function/{name:["+NameExpression+"]+}", faasHandlers.FunctionStatus).Methods(http.MethodGet)
